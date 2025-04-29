4400958ecd02bd7d2b8a01e775613ec13c7b7109
MarquesVitor01 <marque.vitor47@gmail.com>
2025-04-29 09:38:29 -0300

    adapatções de rota/ back-end

--- Server/app.js
+++ Server/app.js
@@ -9,8 +9,8 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    // origin: process.env.FRONTEND_URL || "https://grupomapscartaodigital.com.br",
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "https://future-me-5nhg.vercel.app/",
+    // origin: process.env.FRONTEND_URL || "http://localhost:3000",
   })
 );
 app.use(express.json());
