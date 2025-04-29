6d2a0c794a948c3e3fc713caeab3d1fbf9659c90
MarquesVitor01 <marque.vitor47@gmail.com>
2025-04-29 13:14:35 -0300

    finalizado

--- Server/index.js
+++ Server/index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(
   cors({
     origin: process.env.FRONTEND_URL || "https://future-me-5nhg.vercel.app",
-    // origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
   })
 );
 app.use(express.json());
